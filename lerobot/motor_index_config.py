a5f424ad4dc19c70f8b8dcda964f3b8fe6abaa39
aposadasn <aposadas@mit.edu>
2025-01-16 13:26:08 -0500

    udpate

---
+++ lerobot/motor_index_config.py
@@ -0,0 +1,30 @@
+from lerobot.common.robot_devices.motors.dynamixel import DynamixelMotorsBus
+
+leader_port = "/dev/ttyACM0"
+follower_port = "/dev/ttyACM1"
+
+leader_arm = DynamixelMotorsBus(
+    port=leader_port,
+    motors={
+        # name: (index, model)
+        "shoulder_pan": (1, "xl330-m077"),
+        "shoulder_lift": (2, "xl330-m077"),
+        "elbow_flex": (3, "xl330-m077"),
+        "wrist_flex": (4, "xl330-m077"),
+        "wrist_roll": (5, "xl330-m077"),
+        "gripper": (6, "xl330-m077"),
+    },
+)
+
+follower_arm = DynamixelMotorsBus(
+    port=follower_port,
+    motors={
+        # name: (index, model)
+        "shoulder_pan": (1, "xl430-w250"),
+        "shoulder_lift": (2, "xl430-w250"),
+        "elbow_flex": (3, "xl330-m288"),
+        "wrist_flex": (4, "xl330-m288"),
+        "wrist_roll": (5, "xl330-m288"),
+        "gripper": (6, "xl330-m288"),
+    },
+)
